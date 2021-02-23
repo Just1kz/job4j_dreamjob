c5a032fbb7bff7ae61a78b38ab24f6c658e1a306
Just1lz <exelent.pride@rambler.ru>
2021-02-24 01:11:20 +0700

    1. Написать тест на PostServlet. [#409476]

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -35,41 +35,42 @@ import static org.mockito.Mockito.verify;
 public class PostServletTest {
 
     @Test
-    public void whenDoPost() throws ServletException, IOException {
-        Store store = MemStore.instOf();
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
-        PowerMockito.mockStatic(LoggerFactory.class);
-        Logger mockLOG = mock(Logger.class);
-        PowerMockito.when(org.slf4j.LoggerFactory.getLogger(PsqlStore.class.getName())).thenReturn(mockLOG);
-        PowerMockito.mockStatic(PsqlStore.class);
-        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
-        PowerMockito.when(req.getParameter("id")).thenReturn("0");
-        PowerMockito.when(req.getParameter("name")).thenReturn("Junior");
-        PowerMockito.when(req.getParameter("description")).thenReturn("SberBank");
-        new PostServlet().doPost(req, resp);
-
-        Post result = store.findAllPosts().iterator().next();
-        Assert.assertThat(result.getName(), Is.is("Junior"));
-        Assert.assertThat(result.getDescription(), Is.is("SberBank"));
-    }
-
-    @Test
     public void whenDoGet() throws ServletException, IOException {
-
         Store store = MemStore.instOf();
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
         HttpSession session = mock(HttpSession.class);
         RequestDispatcher dispatcher = mock(RequestDispatcher.class);
-
+        PowerMockito.mockStatic(LoggerFactory.class);
         PowerMockito.mockStatic(PsqlStore.class);
+
         when(PsqlStore.instOf()).thenReturn(store);
         when(req.getSession()).thenReturn(session);
         when(req.getRequestDispatcher(any())).thenReturn(dispatcher);
+
         new PostServlet().doGet(req, resp);
 
-        Assert.assertThat(store.size(), Is.is(0));
+        verify(req).getRequestDispatcher("posts.jsp");
+    }
+
+    @Test
+    public void whenDoPost() throws ServletException, IOException {
+        Store store = MemStore.instOf();
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        PowerMockito.mockStatic(LoggerFactory.class);
+        PowerMockito.mockStatic(PsqlStore.class);
+
+        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
+        PowerMockito.when(req.getParameter("id")).thenReturn("0");
+        PowerMockito.when(req.getParameter("name")).thenReturn("Junior");
+        PowerMockito.when(req.getParameter("description")).thenReturn("SberBank");
+
+        new PostServlet().doPost(req, resp);
+
+        Post result = store.findAllPosts().iterator().next();
+        Assert.assertThat(result.getName(), Is.is("Junior"));
+        Assert.assertThat(result.getDescription(), Is.is("SberBank"));
     }
 
 }
