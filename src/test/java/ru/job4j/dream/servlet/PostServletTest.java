0aa2d2ca7c5883b25d8d12cb58058e8612062602
Just1lz <exelent.pride@rambler.ru>
2021-02-24 00:23:54 +0700

    1. Написать тест на PostServlet. [#409476]

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -1,12 +1,15 @@
 package ru.job4j.dream.servlet;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.spi.LoggerFactory;
 import org.hamcrest.core.Is;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.PsqlStore;
@@ -20,9 +23,15 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(PsqlStore.class)
+@PrepareForTest({PsqlStore.class, LoggerFactory.class})
 public class PostServletTest {
 
     @Test
@@ -30,7 +39,9 @@ public class PostServletTest {
         Store store = MemStore.instOf();
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-
+        PowerMockito.mockStatic(LoggerFactory.class);
+        Logger mockLOG = mock(Logger.class);
+        PowerMockito.when(org.slf4j.LoggerFactory.getLogger(PsqlStore.class.getName())).thenReturn(mockLOG);
         PowerMockito.mockStatic(PsqlStore.class);
         PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
         PowerMockito.when(req.getParameter("id")).thenReturn("0");
@@ -45,6 +56,7 @@ public class PostServletTest {
 
     @Test
     public void whenDoGet() throws ServletException, IOException {
+
         Store store = MemStore.instOf();
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
